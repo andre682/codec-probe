02146285b9fd92d7cf351e6bc19ef25c3f152cce
andre.torbitoni <andre.torbitoni@gupy.com.br>
2021-05-03 12:45:29 -0300

    validation

--- index.js
+++ index.js
@@ -5,19 +5,31 @@ var path = require('path');
 const checkMp4Codec = (videoFile) => {
     const tracks = Mux.mp4.probe.tracks(videoFile)
     console.log(tracks)
-    video = tracks.filter((t) => t.type == 'video')
-    audio = tracks.filter((t) => t.type == 'audio')
+    video = tracks.filter((t) => t.type == 'video')[0]
+    audio = tracks.filter((t) => t.type == 'audio')[0]
+
+    let videoCompativel, audioCompativel
+
+    if (video && video.codec === 'avc1.64001f') {
+        videoCompativel = true
+    }
+    if (audio && audio.codec === 'mp4a.40.2') {
+        audioCompativel = true
+    }
+    console.log(videoCompativel && audioCompativel ? "COMPATIVEL":"INCOMPATIVEL")
 }
+
 
 const videoFolder = './videos'
 fs.readdir(videoFolder, (err, files) => {
-    files.forEach((file, index) => {
-        console.log(file)
+    files.forEach((filename, index) => {
+        console.log(`${index} - filename: ${filename}`)
         try {
-            const videoFile = fs.readFileSync(`${videoFolder}/${file}`)
+            const videoFile = fs.readFileSync(`${videoFolder}/${filename}`)
             checkMp4Codec(videoFile)
         } catch (error) {
             console.log(error)
         }
+        console.log('\n\n')
     })
 })
